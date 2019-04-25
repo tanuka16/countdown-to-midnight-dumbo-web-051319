ad68a6b6fc3aef10fc87e0fe619e664629243d44
Tanuka Das <das.tanuka@yahoo.com>
2019-04-25 14:55:00 +0000

    Done.

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,10 @@
 #write your code here
 
-def countdown
+def countdown(number)
+  number = 0
+  while number == 10
+    puts "#{number} SECOND(S)!"
+    number += 1
+  end
+  return "HAPPY NEW YEAR!"
 end
