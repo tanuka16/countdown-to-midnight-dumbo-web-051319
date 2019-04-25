6f4d30c4419e1d6dc59974bf52c3e0d20ec3c6c3
Tanuka Das <das.tanuka@yahoo.com>
2019-04-25 17:33:51 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -4,7 +4,7 @@ def countdown(number)
   #number = 10
   while number > 0 do                                 #
     puts "#{number} SECOND(S)!"
-    number += 1
+    number -= 1
   end
   return "HAPPY NEW YEAR!"
 end
